768d3c449c37f4554d84d1541323dfa145e858eb
Bartok <thom.huenger@gmail.com>
2021-07-21 00:09:44 -0400

    component/page boiler plate

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Do what you want.
         </a>
       </header>
     </div>
